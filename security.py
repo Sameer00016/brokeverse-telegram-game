d6d9b1c894b7104c4b69b1c9273d4eab8a4f8999
Sameer00016 <sameer031793@gmail.com>
2026-01-25 18:12:20 +0500

    Create security.py

---
+++ security.py
@@ -0,0 +1,25 @@
+import time
+
+def anti_tap(user):
+    now = time.time()
+
+    if "last_tap" not in user:
+        user["last_tap"] = now
+        user["tap_count"] = 1
+        return True
+
+    if now - user["last_tap"] < 1:
+        user["tap_count"] += 1
+    else:
+        user["tap_count"] = 1
+        user["last_tap"] = now
+
+    if user["tap_count"] > 5:
+        user["warnings"] += 1
+        return False
+
+    return True
+
+
+def can_claim(user):
+    return time.time() - user["last_claim"] >= 86400
